aa359e5bc2b480647357a02d7fad938fb6107083
Shubham Mane <123303343+Shubham2511github@users.noreply.github.com>
2023-05-23 00:14:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+var removeColor = document.getElementsByTagName("input")[0];
+
+removeColor.addEventListener("click",remove);
+
+function remove() {
+	var colorset = document.getElementById("colorSelect");
+	var selectedIndex = colorset.selectedIndex;
+	colorset.remove(selectedIndex);
+}
